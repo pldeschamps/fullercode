c3aa59dc397a680ff4cc95bd1adc84e9d41b2896
Pierre-Louis Deschamps <jdd.deschamps@gmail.com>
2025-10-05 22:29:57 +0200

    used new classes and named each face

--- fuller.js
+++ fuller.js
@@ -8,24 +8,26 @@ fetch("icosaedre.json")
         // Création des sommets
         const verts = {};
         data.vertices.forEach(v => {
+            const pt = new GeoPoint(v.id, v.lat, v.lon);
+            //pt.computeXYZ();
             verts[v.id] = Cesium.Cartesian3.fromDegrees(v.lon, v.lat, 0);
             viewer.entities.add({
                 position: verts[v.id],
                 point: { pixelSize: 8, color: Cesium.Color.RED },
-                label: { text: v.name, font: "24px sans-serif", pixelOffset: new Cesium.Cartesian2(0, -12) }
+                label: { text: v.id.toString(), font: "24px sans-serif", pixelOffset: new Cesium.Cartesian2(0, -12) }
             });
         });
 
-        // Création des positions pour chaque face
-        const facesPositions = data.faces.map(face =>
-            face.map(id => Cesium.Cartesian3.fromDegrees(
+        // Création des positions pour chaque face (tableau de GeoPoint)
+        const facesGeoPoints = data.faces.map(face =>
+            face.vertices.map(id => Cesium.Cartesian3.fromDegrees(
                 data.vertices.find(v => v.id === id).lon,
                 data.vertices.find(v => v.id === id).lat,
-                0
+                10
             ))
         );
 
         // Stockage dans l'objet global
-        window.fullerData.facesPositions = facesPositions;
+        window.fullerData.facesPositions = facesGeoPoints;
         window.fullerData.viewer = viewer; // Pour accès dans cesium.js
     });
